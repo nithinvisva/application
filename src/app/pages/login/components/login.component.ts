338983093fd677b2e858386fc44da25dc9f1503c
nithinvisva <visvanic@gmail.com>
2022-07-21 08:52:48 +0530

    login form reset

--- src/app/pages/login/components/login.component.ts
+++ src/app/pages/login/components/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LogIn } from '../models/login.interface';
 import { LoginService } from '../services/login.service';
@@ -9,13 +10,16 @@ import { LoginService } from '../services/login.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('form') loginForm: NgForm
   formValues = {} as LogIn;
   emailPattern=new RegExp('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
   inputType: string = 'password';
   visibilityIcon: string = 'visibility';
 
   constructor(private router:Router,
-    private loginService: LoginService) { }
+    private loginService: LoginService) { 
+      this.loginForm={} as NgForm
+    }
 
   ngOnInit(): void {
   }
@@ -23,9 +27,11 @@ export class LoginComponent implements OnInit {
     this.inputType = this.inputType == 'password' ? 'text' : 'password';
     this.visibilityIcon = this.inputType == 'password' ? 'visibility' : 'visibility_off';
   }
-
-
   onSubmit(){
-    (this.loginService.login(this.formValues))?this.router.navigate(['/home']): this.router.navigate(['/login']) 
+    (this.loginService.login(this.formValues))?this.router.navigate(['/home']): this.formReset() 
+  }
+  formReset(){
+    this.loginForm.resetForm();
+    this.formValues = {} as LogIn;
   }
 }
